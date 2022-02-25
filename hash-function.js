5ba53f72511654babc265900cebc4b4b7ac6ff8d
hamidreza ghahremani <97837767+hamidreza01@users.noreply.github.com>
2022-02-26 00:16:48 +0330

    delete extra files

--- hash-function.js
+++
@@ -1,9 +0,0 @@
-const crypto = require('crypto');
-
-const cryptoHash = (...inputs) => {
-  const hash = crypto.createHash('sha256');
-  const dataHash = hash.update(inputs.sort().join(' '));
-  return dataHash.digest('hex');
-};
-
-module.exports = cryptoHash;
