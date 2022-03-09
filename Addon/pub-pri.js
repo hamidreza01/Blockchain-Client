9f68ef6cdb287948877aa6248a7d75de4b66b7b9
hamidreza ghahremani <97837767+hamidreza01@users.noreply.github.com>
2022-03-09 21:38:17 +0330

    Add files via upload

--- Addon/pub-pri.js
+++ Addon/pub-pri.js
@@ -1,4 +1,10 @@
-const EC = require('elliptic').ec;
-const ec = new EC();
+const cryptoHash = require('./hash-function');
 
-module.exports = ec;
+const EC = require('elliptic').ec;
+const ec =  new EC('secp256k1');
+
+const verifySign = ({data , sign , publicKey})=>{
+    const vrf = ec.keyFromPublic(publicKey,'hex');
+    return vrf.verify(cryptoHash(data),sign)
+}
+module.exports = {ec , verifySign};
