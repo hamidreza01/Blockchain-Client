837fa5fb6c683b683fa2b3db59916a7b57e86b11
hamidreza ghahremani <97837767+hamidreza01@users.noreply.github.com>
2022-03-09 21:39:08 +0330

    Add files via upload

--- Currency/wallet.js
+++ Currency/wallet.js
@@ -1,15 +1,15 @@
-// let { defBalance } = require("../config");
-// const ec = require("../Addon/pub-pri");
-// const cryptoHash = require("../Addon/hash-function");
-// class Wallet {
-//   constructor() {
-//     this.balence = defBalance;
-//     this.keyPair = ec.keyPair();
-//     this.publicKey = this.keyPair.getPublic().encode("hex");
-//   }
-//   sign(data) {
-//     return this.keyPair.sign(cryptoHash(dara));
-//   }
-// }
+let { defBalance } = require("../config");
+const {ec} = require("../Addon/pub-pri");
+const cryptoHash = require("../Addon/hash-function");
+class Wallet {
+  constructor() {
+    this.balance = defBalance;
+    this.keyPair = ec.genKeyPair();
+    this.publicKey = this.keyPair.getPublic().encode("hex");
+  }
+  sign(data) {
+    return this.keyPair.sign(cryptoHash(data));
+  }
+}
 
-// module.exports = Wallet;
+module.exports = Wallet;
