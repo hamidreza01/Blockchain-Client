29b9cc0bc0a7e5ffb653ac69bea468c546fc072b
hamidreza ghahremani <97837767+hamidreza01@users.noreply.github.com>
2022-04-03 18:31:15 +0430

    Delete _Transcation.ts

--- Src/interfaces/Blockchain/_Transcation.ts
+++
@@ -1,8 +0,0 @@
-import { Wallet_types } from "../../types/Wallet_types";
-
-export interface _Transcation {
-    id : string,
-    inputMap : {},
-    outputMap : {},
-    inputMapCreator : (senderWallet : Wallet_types)
-}
