d59df5a74105f536f6ee47d4c98649b22e1335f9
hamidreza ghahremani <97837767+hamidreza01@users.noreply.github.com>
2022-04-03 18:06:30 +0430

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,1 @@
-import { Wallet } from "./Src/classes/Blockchain/Wallet";
-import { Transaction } from "./Src/classes/Blockchain/Transaction";
-const wallet = new Wallet();
-const transaction = new Transaction(wallet,10,'teiajsdijasd213asdiohasoidhiasdias123st')
-console.log(Transaction.isValid(transaction))
+
