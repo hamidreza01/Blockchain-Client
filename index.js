d1fb480c94e77a2984bea6f738457e869799b4e8
hamidreza ghahremani <97837767+hamidreza01@users.noreply.github.com>
2022-04-03 18:06:47 +0430

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-"use strict";
-exports.__esModule = true;
-var Wallet_1 = require("./Src/classes/Blockchain/Wallet");
-var Transaction_1 = require("./Src/classes/Blockchain/Transaction");
-var wallet = new Wallet_1.Wallet();
-var transaction = new Transaction_1.Transaction(wallet, 10, 'teiajsdijasd213asdiohasoidhiasdias123st');
-console.log(Transaction_1.Transaction.isValid(transaction));
+
