89f9ada03033fd2bf9097082b5cce506048857df
hamidreza ghahremani <97837767+hamidreza01@users.noreply.github.com>
2022-04-27 17:19:01 +0430

    Delete command.js

--- App/command.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// class Command{
-//     private classData : Array<{bet : string, callBack : Function}> = []; 
-//     constructor(){
-//     }
-//     bet(bet : string , callBack : Function){
-//         this.classData.push({bet : bet, callBack : callBack});
-//     }
-// }
